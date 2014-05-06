9a6b9c3db1ddcaa295a62d9b361fbaed197b2305
DedovetsPavel <dedovets.pavel4@gmail.com>
2014-05-06 12:40:08 +0300

    delete idea folder
    Signed-off-by: DedovetsPavel <dedovets.pavel4@gmail.com>

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Мой бухгалтер1',
+	'name'=>'Мой бухгалтер11111',
     'language' => 'ru',
 
 	// preloading 'log' component
